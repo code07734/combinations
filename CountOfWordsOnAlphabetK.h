b74caefeaecfdba5ed39ca3e41ae9c258e293dcc
Vladimir <47279914+code07734@users.noreply.github.com>
2023-05-09 02:24:33 +0700

    Update CountOfWordsOnAlphabetK.h

--- CountOfWordsOnAlphabetK.h
+++ CountOfWordsOnAlphabetK.h
@@ -9,38 +9,27 @@ u64 pwr(u64 x, u64 n){
 	return xx;
 }
 
-u64 *CountOfWordsOnAlphabetK(u64 wordLen, u64 alphabetLen, u64 *K, u64 *countOfWords){
-	if(alphabetLen>wordLen){return 0;}
-	u64 lim=0, z_in_n=1;
+void CountOfWordsOnAlphabetK(u64 alphabetLen, u64 wordLen, u64 subAlphabetLen, u64 *count){
+	u64 combsOfi=1,combsOfSubi=1,*countOfSubA,*countOfWords,*countOfAllWords;
+	countOfSubA=count+0x2000;
+	countOfWords=count+0x1000;
+	countOfAllWords=count;
 
-	for (u64 n = 0; n < alphabetLen; n+=1)
+	for (u32 i = 0; i < subAlphabetLen; i++)
 	{
-		countOfWords[n]=pwr(n+1,wordLen);
-		K[n]=countOfWords[n];
+		combsOfi=combsOfi*(alphabetLen-i)/(i+1);
+		countOfSubA[i]=combsOfi;
+		
+		countOfWords[i]=pwr(i+1,wordLen);
 
-		for (u64 z = 0; z < n; z+=1)
+		combsOfSubi=1;
+		
+		for (u32 n = 0; n < i; n+=1)
 		{
-			z_in_n = 1;
-			//printf("%llu %llu % llu\n:", (n+1), (n-z), (z+1));
-
-			for(u64 zc=0;zc<(z+1);zc+=1){
-				z_in_n*=((n+1)-zc);
-				z_in_n/=(zc+1);
-			}
-			//printf("%llu\n", z_in_n);
-
-			countOfWords[n]-=countOfWords[z]*z_in_n;
-			if(n==(alphabetLen-1)){
-				K[lim]=z_in_n;
-				lim+=1;
-			}
+			combsOfSubi=combsOfSubi*(i+1-n)/(n+1);
+			countOfWords[i]-=countOfWords[n]*combsOfSubi;
 		}
-		K[lim]=1;
+		
+		countOfAllWords[i]=countOfWords[i]*combsOfi;
 	}
-	for (u64 i = 0; i < alphabetLen; i+=1)
-	{
-		countOfWords[i]*=K[i];
-	}
-	//printf("\n\n");
-	return countOfWords;
 }
