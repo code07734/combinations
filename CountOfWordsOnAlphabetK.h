939cafb21fa885f4da7d9eb6aec014916d239160
Vladimir <47279914+code07734@users.noreply.github.com>
2023-05-10 21:46:18 +0700

    Update CountOfWordsOnAlphabetK.h

--- CountOfWordsOnAlphabetK.h
+++ CountOfWordsOnAlphabetK.h
@@ -9,27 +9,26 @@ u64 pwr(u64 x, u64 n){
 	return xx;
 }
 
-void CountOfWordsOnAlphabetK(u64 alphabetLen, u64 wordLen, u64 subAlphabetLen, u64 *count){
-	u64 combsOfi=1,combsOfSubi=1,*countOfSubA,*countOfWords,*countOfAllWords;
-	countOfSubA=count+0x2000;
-	countOfWords=count+0x1000;
-	countOfAllWords=count;
+void NOfSubStrsWithKdistinctsEls(u64 alphabetLen, u64 wordLen, u64 subAlphabetLen, u64 *numb){
+	u64 combsOfi=1,combsOfSubi=1,*NOfSubA,*NOfWords,*NOfAllWords;
+	NOfSubA=numb+0x2000;
+	NOfWords=numb+0x1000;
+	NOfAllWords=numb;
 
 	for (u32 i = 0; i < subAlphabetLen; i++)
 	{
 		combsOfi=combsOfi*(alphabetLen-i)/(i+1);
-		countOfSubA[i]=combsOfi;
+		NOfSubA[i]=combsOfi;
 		
-		countOfWords[i]=pwr(i+1,wordLen);
+		NOfWords[i]=pwr(i+1,wordLen);
 
 		combsOfSubi=1;
-		
 		for (u32 n = 0; n < i; n+=1)
 		{
 			combsOfSubi=combsOfSubi*(i+1-n)/(n+1);
-			countOfWords[i]-=countOfWords[n]*combsOfSubi;
+			NOfWords[i]-=NOfWords[n]*combsOfSubi;
 		}
 		
-		countOfAllWords[i]=countOfWords[i]*combsOfi;
+		NOfAllWords[i]=NOfWords[i]*combsOfi;
 	}
 }
